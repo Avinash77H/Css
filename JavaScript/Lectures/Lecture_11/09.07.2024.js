96d9ce37eef1dda4f9704f94aaa71d9516760fd6
Avinash77H <arakholiya5@gmail.com>
2024-07-09 17:40:15 +0530

    09.07.2024

---
+++ JavaScript/Lectures/Lecture_11/09.07.2024.js
@@ -0,0 +1,60 @@
+let x = "1050";
+
+/* convert string into number */
+// console.log((+x));
+// console.log(Number(x));
+// console.log(Number(x));
+
+
+
+let y = 112.57898787;
+
+
+// console.log(y);
+
+/* convert number into string */
+
+/**  The toString() method of Number values returns a string representing this number value. **/
+
+// console.log(y.toString());
+// console.log(y1.toString());
+
+/** The toExponential() method of Number values returns a string representing this number in exponential notation. **/
+
+// console.log(y.toExponential());
+// console.log(y.toExponential(2));
+// console.log(y.toExponential(4));
+
+/** The toPrecision() method of Number values returns a string representing this number to the specified number of significant digits. **/
+
+// console.log(y.toPrecision());
+// console.log(y.toPrecision(5));
+
+/**  The toFixed() method of Number values returns a string representing this number using fixed-point notation with the specified number of decimal places. **/
+
+// console.log(y.toFixed());
+// console.log(y.toFixed(1));
+// console.log(y.toFixed(3));
+
+/** The valueOf() method of Number values returns the value of this number. **/
+
+
+// console.log(y.valueOf());
+
+
+/* convert string into number */
+
+// console.log(parseInt("10"));
+// console.log(parseInt("10.33"));
+// console.log(parseInt("10 "));
+// console.log(parseInt(" 10 "));
+// console.log(parseInt("10yes"));
+// console.log(parseInt('yes 10'));
+// console.log(parseFloat("10.34343"));
+// console.log(parseFloat("yes 10"));
+
+
+console.log(Number.isInteger(123456576654433434332));
+
+console.log(Number.isSafeInteger(9876987698798769876));
+console.log(Number.isSafeInteger(987698769879876));
