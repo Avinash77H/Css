239cecdd2b38be39a39cdd825a9f8f6a7e2529ed
Avinash77H <arakholiya5@gmail.com>
2024-07-12 22:48:27 +0530

    12.07.2024

--- JavaScript/Lectures/Lecture_14/12.07.2024.js
+++ JavaScript/Lectures/Lecture_14/12.07.2024.js
@@ -50,15 +50,88 @@
 /* this keyword */
 
 {
-    let person = {
-        firstName : 'Virat',
-        lastName : 'Kohli',
-        age : 34,
-        hobbies : ['Cricket', 'Music', 'Dancing'],
-        test: function (){
-            console.log(`Player Info: ${this.firstName} ${this.lastName}.
-                his age is ${this.age} years old and his hobbies is ${this.hobbies[0]}`);
-        }
-    }
-    person.test();
+    // let person = {
+    //     firstName : 'Virat',
+    //     lastName : 'Kohli',
+    //     age : 34,
+    //     hobbies : ['Cricket', 'Music', 'Dancing'],
+    //     test: function (){
+    //         console.log(`Player Info: ${this.firstName} ${this.lastName}.
+    //             his age is ${this.age} years old and his hobbies is ${this.hobbies[0]}`);
+    //     }
+    // }
+    // person.test();
 }
+
+
+
+/** Revision Practice **/
+
+{
+    /* function as object */
+
+    // function sumAll(){
+
+    //     console.log(`argument length is: ${arguments.length}`);
+    //     let sum = 0;
+    //     for(let i = 0; i < arguments.length; i++){
+    //         sum += arguments[i];
+    //     }
+    //     console.log(sum);
+
+        
+    // }
+
+    //  sumAll(1,2,3);
+
+    
+}
+
+{
+    /* rest parameter */
+
+    // function restPara(... args){
+    //     return args;
+    // }
+
+    // console.log(restPara(2,3,6,8,9));
+    // console.log(typeof restPara());
+}
+
+{
+    /* Generator */
+
+    // function* generator(a , b){
+    //     yield 1;
+    //     yield  `sum of a and b is : ${a + b}`;
+    //     yield 3;
+    //     yield 4;
+    //     yield "hello";
+    //     return "jay shree ram";
+    // }
+
+    // let x = generator(5,5);
+
+    // console.log(x.next().value);
+    // console.log(x.next().value);
+    // console.log(x.next().value);
+    // console.log(x.next().value);
+    // console.log(x.next().value);
+    // console.log(x.next().value);
+    // console.log(x.next().value);
+}
+
+{
+    /* this keyword */
+
+    // let obj ={
+    //     name : 'avinash',
+    //     age : 23,
+    //     intro : function(){
+    //         return(`My name is  ${this.name} rakholiya and i am ${this.age} year old `);
+    //     }
+    // }
+
+    // console.log(obj.intro());
+}
+
--- JavaScript/practice/practice-4/practice.js
+++ JavaScript/practice/practice-4/practice.js
@@ -229,3 +229,112 @@
     // leapYear(2204);
 }
 
+/* 12. Write a JS function that reverse a number. Example x = 15438; */
+
+{
+    // function reverseNum(num){
+    //     //convert number into string
+    //    let numStr = num.toString();
+    //    // convert string into array
+    //    // reverse the  array
+    //    // all element of array join like string
+    //    let reverseStr = numStr.split('').reverse().join('');
+    //     // convert string into interger
+    //    let reverseNumber = parseInt(reverseStr,10);
+    //     return reverseNumber;
+    // }
+
+    // console.log(reverseNum(12345));
+}
+
+
+/* 13. Write a JS function that returns a passed string with letters in alphabetical order. 
+Example string : 'webmaster'
+Expected Output : 'abeemrstw'
+Assume punctuation and numbers symbols are not included in the passed string. */
+
+
+{
+    // function sortStr(str){
+    //     str = str.toLowerCase();
+
+    //    let charArray = str.split('').sort().join('');
+    
+    //    return charArray;
+    // }
+
+    // console.log(sortStr("Avinash"));
+}
+
+
+/* 14. Write a JS function that accepts a string as a parameter and converts the first letter of each word of the string in upper case. 
+Example string : 'the quick brown fox'
+Expected Output : 'The Quick Brown Fox' */
+
+{
+    // function firstCharCapitalize(str){
+    //     wordArray = str.split(' ');
+    //     for(let i = 0; i < wordArray.length; i++){
+    //         wordArray[i] = wordArray[i].charAt(0).toUpperCase() + wordArray[i].slice(1);
+    //     }
+
+    //     capitalStr = wordArray.join(' ');
+
+    //     return capitalStr;
+        
+    // }
+    // console.log(firstCharCapitalize('jay shree ram'));
+}
+
+/* 15. Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
+Example string : 'Web Development Tutorial'
+Expected Output : 'Development' */
+
+{
+    // function longStr(str){
+    //     let charArray = str.split(' ');
+    //     let newArray = [];
+        
+
+    //     for(let i = 0; i < charArray.length; i++){
+    //         newArray[i] = charArray[i].length;
+    //     }
+
+    //     let count = 0;
+    //     for(let i = 0; i < newArray.length; i++){
+    //         if(count < newArray[i]){
+    //             count = newArray[i];
+    //         }
+    //     }
+
+    //     for(let i = 0; i < newArray.length; i++){
+    //         if(charArray[i].length == count){
+    //             return charArray[i];
+    //         }
+    //     }
+        
+        
+    // }
+    // console.log(longStr('Bolo Siyavar Ramchandra ki jay'));
+}
+
+/* 16. Write a JavaScript function that checks whether a number is perfect. */
+
+{
+    // function perfectNumber(num){
+    //     if(num <= 1){
+    //         return false;
+    //     }
+
+    //     let sum = 0;
+
+    //     for(let i = 1; i <= num / 2; i++){
+    //         if(num % i == 0){
+    //             sum += i;
+    //         } 
+    //     }
+    //     return sum == num;
+    // }
+
+    // console.log(perfectNumber(8128));
+}
